0b1c0f21a253f4e9a503db9ba5a9f1f358b3268a
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-13 19:11:44 +0100

    Ejercicio3
    calcula el factorial de un número

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
-void main() {
+unsigned long long Factorial(unsigned long long num) {
+	long long factorial = 0;
+	if (num != 1) {
+		return num * Factorial(num - 1);
+	}
 
+}
+
+void main() {
+	unsigned long long num;
+	std::cout << "Ponga un numero para factorizar: ";
+	std::cin >> num;
+
+	unsigned long long resultado = Factorial(num);
+	std::cout << resultado;
 }
