0c6ed1a3a1ce330dd6342cf518c8a535052f981e
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-13 22:29:53 +0100

    Ejercicio4
    ahora si esta hecho

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 
-void Exponentes(int &num, int &num1, int n) {
-
-	if (n != 1) {
-		return 
+double Exponentes( double base, int exponente) {
+	if (exponente == 0) {
+		return 1;
 	}
-
+	return base * Exponentes(base, exponente -1);
 }
 
 
 void main() {
-	int num;
-	int num1;
-
-	int n;
+	int base;
+	int exponente;
 	std::cout << "Ponga un numero para la base: ";
-	std::cin >> num;
+	std::cin >> base;
 	std::cout << "Ponga un numero para el exponente: ";
-	std::cin >> num1;
-	n = num1;
-	Exponentes(num, num1, n);
+	std::cin >> exponente;
+	unsigned long long resultado = Exponentes(base, exponente);
+	std::cout << "El resultado es este: " << resultado;
 }
