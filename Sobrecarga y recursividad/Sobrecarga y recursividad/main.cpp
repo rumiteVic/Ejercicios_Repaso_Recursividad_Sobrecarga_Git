25a8c971b0151fc99bb995642726717bc6a1a588
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-13 18:40:53 +0100

    Ejercicio1
    el ejercicio1

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+void sumen(int& num, int& n, int& suma) {
+    if (n != (num + 1)) {
+        suma += n;
+        std::cout << suma << "\n";
+        n++;
+        sumen(num, n, suma);
+    }
+}
 
 void main() {
-
+    int num;
+    int n = 0;
+    int suma = 0;
+    std::cout << "Pon un numero: ";
+    std::cin >> num;
+    sumen(num, n, suma);
 }
