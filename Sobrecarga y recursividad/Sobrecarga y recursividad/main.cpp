9021df474248d56d73fd9729d42e33dfbc54b9fe
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-13 18:50:37 +0100

    Ejercicio2
    el ejercicio 2

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+void CuentaAtras(int num) {
+    if (num != 0) {
+        std::cout << num << "\n";
+        CuentaAtras(num -1);
+    }
+}
 
 void main() {
-
+    int num;
+    std::cout << "Pon un numero: ";
+    std::cin >> num;
+    CuentaAtras(num);
 }
