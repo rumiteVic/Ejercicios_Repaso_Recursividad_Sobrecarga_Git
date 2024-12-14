8744d6293a9696fe7e098025768573333870f44a
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-14 15:22:22 +0100

    ejercicio6 fin
    30 minutos y una cara de dina4 intentando entender fibonacci mediante internet, si

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
 
-
-
-unsigned long Fibonacci(unsigned long num, unsigned long&total, unsigned long&total2) {
-	int n = 0;
+unsigned long Fibonacci(unsigned long num, unsigned long &anteriorAnterior, unsigned long &anterior, unsigned long &actual, unsigned long n) {
 	if (n != num) {
-		return n;
+		if (num == 0) {
+			return 0;
+		}
+		else if (num == 1) {
+			return 1;
+		}
+		else {
+			actual = anterior + anteriorAnterior;
+			std::cout << actual << "\n";
+			anteriorAnterior = anterior;
+			anterior = actual;
+			n++;
+			return actual;
+		}
 	}
+	
 }
 
 
 void main() {
 	unsigned long num;
-	unsigned long total = 0;
-	unsigned long total2 = 0;
+	unsigned long anteriorAnterior = 0;
+	unsigned long anterior = 1;
+	unsigned long actual;
+	unsigned long n = 2;
 	std::cout << "Ponga un numero: ";
 	std::cin >> num;
-	if (num != 0 && num != 1 && num != 2) {
-		Fibonacci(num, total, total2);
+	if (num == 0) {
+		std::cout << "0\n";
 	}
-	else if (num == 0) {
-		std::cout << "0";
+	for (int a = 0; a < num; a++) {
+		if (a == 0) {
+			std::cout << "0\n";
+		}
+		if (a == 1) {
+			std::cout << "1\n";
+		}
+		Fibonacci(num, anteriorAnterior, anterior, actual, n);
 	}
-	else if (num == 1) {
-		std::cout << "0\n1";
-	}
-	else if (num == 2) {
-		std::cout << "0\n1\n1";
-	}
-	
-
 }
