a0f965b1353584ce4fc55a8320c73dd7ceff77d9
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-14 02:05:29 +0100

    ejercicio5
    el ejercicio 5 hecho

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+bool Primo(int num, int divisor) {
+	if (divisor == 1) {
+		return true;
+	}
+	if (num % divisor == 0) {
+		return false;
+	}
+	return Primo(num, divisor - 1);
+}
+
 void main() {
+	int num;
+	int divisor;
+	std::cout << "Pon un numero para saber si es primo o no: ";
+	std::cin >> num;
+	divisor = num - 1;
+
+	if (Primo(num, num - 1)) {
+		std::cout << "Es primo";
+	}
+	else {
+		std::cout << "No es primo";
+	}
+
 
 }
