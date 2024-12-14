bfb3e119bfd6610d57c06a210158b975eefe291e
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-14 17:00:14 +0100

    ejercicio7
    el ejercicio 7 acabado

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <vector>
+
+void what(char &input, bool &nomas, int &n, std::vector<char> &lista) {
+	if (!nomas) {
+		lista.push_back(input);
+		n = lista.size();
+	}
+	else {
+		for (n > 0; n--;) {
+			if (n != 0) {
+				std::cout << lista[n] << "\n";
+			}
+		}
+	}
+}
 
 void main() {
-
+	bool nomas = false;
+	char input;
+	int n = 0;
+	std::vector<char> lista;
+	std::cout << "Ponga cualquier cosa, ponga 0 si se ha cansado de poner caracteres:\n";
+	while (!nomas) {
+		std::cin >> input;
+		if (input == '0') {
+			nomas = true;
+		}
+		what(input, nomas, n, lista);
+	}
 }
