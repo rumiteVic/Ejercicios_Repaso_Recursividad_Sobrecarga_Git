971193e2f30221b997fdce2bfe985d96067e5089
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-16 17:25:15 +0100

    ejercicio8
    hoy he aprendido que - '0' es para pasar de '5' a 5

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
-void main() {
+unsigned int Cadena(std::string cadenaNum, int &cantidad, unsigned int &num) {
 
+	if (cantidad >= 0) {
+		num += cadenaNum[cantidad] - '0';
+		cantidad--;
+		Cadena(cadenaNum, cantidad, num);
+	}
+	return num;
+
+}
+
+void main() {
+	std::string cadenaNum;
+	int cantidad = 0;
+	unsigned int num = 0;
+	unsigned suma = 0;
+	std::cout << "Ponga un numero: ";
+	std::cin >> cadenaNum;
+	cantidad = cadenaNum.size() - 1;
+	Cadena(cadenaNum, cantidad, num);
+	std::cout <<"Toma la suma de esos numeros es " << num;
 }
