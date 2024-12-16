65381ec45e4b0628698564dffb7ac3fa79a23a03
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-17 00:30:25 +0100

    Ejercicio10
    me olvide de para que puse sizeVector y me invente la variable n como si fuera el sizeVector, en fin

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <vector>
+
+bool si(int &input, std::vector<int> vector, int sizeVector) {
+	if (sizeVector != 0) {
+		if (vector[sizeVector] == vector[0]) {
+			if (sizeVector == 0 && vector[sizeVector] == vector[0]) {
+				return true;
+			}
+			sizeVector--;
+			si(input, vector, sizeVector);
+		}
+		else {
+			return false;
+		}
+	}
+
+}
 
 void main() {
+	std::vector<int> vector;
+	int input;
+	int sizeVector;
 
+	bool ya = false;
+	std::cout << "Ponga 0 para dejar de poner caracteres\nY ponga el mismo caracter todo el rato ";
+	while (!ya) {
+		std::cin >> input;
+		if (input == 0) {
+			ya = true;
+			sizeVector = vector.size() -1;			
+		}
+		vector.push_back(input);
+	}
+	if (si(input, vector, sizeVector)) {
+		std::cout << "Todo tiene el mismo valor\n";
+	}
+	else {
+		std::cout << "No tiene el mismo valor\n";
+	}
 }
