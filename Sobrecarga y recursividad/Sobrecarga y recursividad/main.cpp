8e5fe20d854aefd0ade457e225cf4b395986fec2
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-12-16 18:10:03 +0100

    ejercicio9
    comprueba palindromos 👍

--- Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
+++ Sobrecarga y recursividad/Sobrecarga y recursividad/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
-void main() {
+bool palindromo(std::string palabra, int &cantidadLetras, int &cantidadComprobar) {
+	if (cantidadComprobar < palabra.size()) {
+		if (palabra[cantidadComprobar] != palabra[cantidadLetras]) {
+			return false;
+		}
+		else {
+			cantidadComprobar++;
+			cantidadLetras--;
+			if (cantidadLetras == 0 && palabra[cantidadComprobar] == palabra[cantidadLetras]) {
+				return true;
+			}
+			palindromo(palabra, cantidadLetras, cantidadComprobar);
+			
+		}
+	}
+}
 
+void main() {
+	std::string palabra;
+	int cantidadLetras;
+	int cantidadComprobar = 0;
+	std::cout << "Ponga una palabra ";
+	std::cin >> palabra;
+	cantidadLetras = palabra.size() -1;
+	if (palindromo(palabra, cantidadLetras, cantidadComprobar)) {
+		std::cout << "Es un palindromo";
+	}
+	else {
+		std::cout << "No es un palindromo";
+	}
+		
 }
